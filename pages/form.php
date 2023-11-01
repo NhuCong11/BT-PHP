7b4c9e223cc8697181d5fdbc8ee81f134bca1f43
Công <yeusangtao96@gmail.com>
2023-11-01 13:08:38 +0700

    handlesubmit form
    Xử lý gửi form nhượng quyền

---
+++ pages/form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/images/NhuongQuyen/logo_icon.png" type="image/x-icon" />
+    <title>Successfully</title>
+    <link rel="stylesheet" href="../css/form.css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="success">
+            <div class="icon"></div>
+
+            <?php
+            $name = $_POST['name'];
+            if (empty($name)) {
+                echo "<h1 class='title'>Chúc mừng bạn</h1>";
+            } else {
+                echo "<h1 class='title'>Chúc mừng {$name}</h1>";
+            }
+            ?>
+
+            <p class="successfully">đã thành công đăng ký nhượng quyền</p>
+
+            <?php
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            if (empty($email) || empty($phone)) {
+                echo "<p class='desc'>Chúng tôi sẽ sớm hệ với bạn qua email hoặc số điện thoại bạn đã đăng ký</p>";
+            } else {
+                echo "<p class='desc'>Chúng tôi sẽ sớm hệ với bạn qua email: {$email} hoặc số điện thoại: {$phone}</p>";
+            }
+            ?>
+
+            <a href="../index.php" class="home">Quay về trang chủ</a>
+        </div>
+    </div>
+</body>
+
+</html>
