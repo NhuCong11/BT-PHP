c3033dce89c550e4f8befce63c4afa0b72b8546a
Công <yeusangtao96@gmail.com>
2023-11-20 19:25:18 +0700

    initial state
    Tưởng bug, quay lại như ban đầu

--- pages/Order.php
+++ pages/Order.php
@@ -429,6 +429,7 @@
             class="fa-sharp fa-solid fa-arrow-up"></i></a>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script type="module" src="../javascript/order.js"></script>
 </body>
 
 </html>
